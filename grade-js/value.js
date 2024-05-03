5b1cd895e5967cfd5be95170bc7544b7db5bf835
mim-razowana <razowanamim5671@gmail.com>
2024-05-04 02:08:30 +0600

    version2

--- grade-js/value.js
+++ grade-js/value.js
@@ -6,7 +6,9 @@ function grade() {
     const score = parseInt(score1.value);
     console.log(typeof score);
 
-    if (score <= 100 && score >= 80) {
+    if (score > 100 || score < 0) {
+        result.innerHTML = '<h1>Mark is not valid</h1>';
+    } else if (score <= 100 && score >= 80) {
         result.innerHTML = '<h1>A+</h1>';
     } else if (score <= 79 && score >= 70) {
         result.innerHTML = '<h1> A </h1>';
@@ -16,7 +18,7 @@ function grade() {
         result.innerHTML = '<h1> B </h1>';
     } else if (score <= 49 && score >= 33) {
         result.innerHTML = '<h1> C </h1>';
-    } 
+    }
     else {
 
         result.innerHTML = '<h1> F </h1>';
